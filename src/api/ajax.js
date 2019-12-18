22da113c9bf580fa78181f0b47e4341d48fd127a
tms_ych <1925118530@qq.com>
2019-12-18 10:58:02 +0800

    init gshop

---
+++ src/api/ajax.js
@@ -0,0 +1,33 @@
+/*ajax请求函数模块*/
+import axios from 'axios'
+export default function ajax(url,data={},type='GET') {
+  return  new Promise(function (resolve,reject) {
+    //执行异步ajax请求
+    let promise
+    if (type === 'GET') {
+      // 准备 url query 参数数据
+      let dataStr = ''
+      // 数据拼接字符串 O
+      Object.keys(data).forEach(key => {
+        dataStr += key + '=' + data[key] + '&'
+      })
+      if (dataStr !== '') {
+        dataStr = dataStr.substring(0, dataStr.lastIndexOf('&'))
+        url = url + '?' + dataStr
+      }
+      // 发送 get 请求
+      promise = axios.get(url)
+    } else {
+      //发送 post 请求
+      promise = axios.post(url, data)
+    }
+    promise.then(function (response) {
+      //成功了调用resolve()
+      resolve(response.data)
+    }).catch(function (error) {
+      //失败了调用reject()
+      reject(error)
+      })
+  })
+}
+
