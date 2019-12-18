24c7313e2766e7b2ab690b30851fddaf015749cd
tms_ych <1925118530@qq.com>
2019-12-18 13:58:09 +0800

    解决跨域

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,12 @@
 
 <script>
   import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+  import {reqFoodTypes} from './api'
   export default {
+    async mounted(){
+      const result = await reqFoodTypes()
+      console.log(result)
+    },
     components:{
       FooterGuide
     }
